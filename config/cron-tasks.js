5d86957b887ef8e34447d14f72283978769bc229
Abdullah Koroglu <koroglu05abdullah@gmail.com>
2023-02-22 15:45:51 +0300

    new entities

---
+++ config/cron-tasks.js
@@ -0,0 +1,16 @@
+module.exports = {
+  /**
+   * Simple example.
+   * Every monday at 1am.
+   */
+
+  // '*/2 * * * * *': ({ strapi }) => {
+  //   // Add your own logic here (e.g. send a queue of email, create a database backup, etc.).
+  //   console.log ('iyi')
+  // },
+  // '*/3 * * * * *': ({ strapi }) => {
+  //   // Add your own logic here (e.g. send a queue of email, create a database backup, etc.).
+  //   console.log ('naber')
+  //   const users = strapi.query
+  // },
+};
